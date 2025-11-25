43154e21e991135e78f509b937de3143e2a74df7
ZoraizDev <zoraiz.developer@gmail.com>
2025-11-25 20:01:31 +0500

    add key

--- lib/app/resources/api_url/api_url_constants.dart
+++ lib/app/resources/api_url/api_url_constants.dart
@@ -2,6 +2,5 @@ class ApiUrlConstants {
   static const String baseUrl =
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=";
 
-  static const String apiKey = "AIzaSyAHrbWGg_wWalvAYnoOzXSFIvtrhc6ekD0";
+  static const String apiKey = "";
 }
-
